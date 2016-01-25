081ad45f2dc2cd929aca152c03ce3d8fb9d49cb6
Tomek Główka <tk.glowka@gmail.com>
2016-01-25 15:54:17 +0100

    Better executed code testing.

--- ascore_js/aeditor/tests-code.js
+++ ascore_js/aeditor/tests-code.js
@@ -9,37 +9,47 @@ if (!testedFunc) {
 const testCases = [
   // none
   { data: { paidVacation: 31, hammocksInOffice: true, freeLunch: true },
-    avoidScore: 0, error: 'no awesomeness? you can find it, it\'s there' },
+    avoidScore: 0,
+    error: 'no awesomeness? you can find it, it\'s there' },
   { data: { paidVacation: 0, hammocksInOffice: false, freeLunch: false },
-    expectScore: 0, error: 'so yous say that even crappy jobs are awesome...' },
+    expectScore: 0,
+    error: 'so yous say that even crappy jobs are awesome...' },
 
   // vacation
   { data: { paidVacation: 29, hammocksInOffice: false, freeLunch: false },
-    expectScore: 0, error: 'so you think these are decent vacations?' },
+    expectScore: 0,
+    error: 'so you think these are decent vacations?' },
   { data: { paidVacation: 31, hammocksInOffice: false, freeLunch: false },
-    expectScore: 1, error: 'didn\'t you forget about vacations?' },
+    expectScore: (score) => score >= 1,
+    error: 'didn\'t you forget about vacations?' },
   // hammocks
   { data: { paidVacation: 0, hammocksInOffice: true, freeLunch: false },
-    expectScore: 1, error: 'hammock in office will make anyone happy' },
+    expectScore: (score) => score >= 1,
+    error: 'hammock in office will make anyone happy' },
   // lunch
   { data: { paidVacation: 0, hammocksInOffice: false, freeLunch: true },
-    expectScore: 1, error: 'nice job is, where lunch is free' },
+    expectScore: (score) => score >= 1,
+    error: 'nice job is, where lunch is free' },
 
   // all
   { data: { paidVacation: 31, hammocksInOffice: true, freeLunch: true },
-    expectScore: 3, error: 'you aim high, that\'s great, but better check your code' },
-
+    expectScore: (score) => score >= 3,
+    error: 'you aim high, that\'s great, but better check your code' }
 ];
 
-for (const testCase of testCases) {
-  if (typeof testCase.expectScore !== 'undefined') {
-    if (testedFunc(testCase.data) === testCase.expectScore) {
+for (const { expectScore, avoidScore, data, error } of testCases) {
+  if (typeof expectScore !== 'undefined') {
+    if (typeof expectScore === 'function') {
+      if (expectScore(testedFunc(data))) { continue; }
+    } else if (testedFunc(data) === expectScore) {
       continue;
     }
-  } else if (typeof testCase.avoidScore !== 'undefined') {
-    if (testedFunc(testCase.data) !== testCase.avoidScore) {
+  } else if (typeof avoidScore !== 'undefined') {
+    if (typeof avoidScore === 'function') {
+      if (!avoidScore(testedFunc(data))) { continue; }
+    } else if (testedFunc(data) !== avoidScore) {
       continue;
     }
   }
-  throw new EvalError(testCase.error);
+  throw new EvalError(error);
 }
