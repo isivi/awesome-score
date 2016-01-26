4397fa01da4b5b86931dbeceff826c3af1aa283d
Tomek Główka <tk.glowka@gmail.com>
2016-01-26 01:42:54 +0100

    Added missing staticfiles finders

--- ascore/settings/base.py
+++ ascore/settings/base.py
@@ -91,6 +91,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    
     # compressor won't work without it when django.contrib.staticfiles enabled
     'compressor.finders.CompressorFinder',
 )
