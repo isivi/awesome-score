5653dafd425d67a1ebe9b0f734f8f49bf7e4d0db
Konrad Słoniewski <konrad.sloniewski@gmail.com>
2016-02-08 14:43:36 +0100

    Small changes in editor's initial code

--- ascore_js/aeditor/initial-code.js
+++ ascore_js/aeditor/initial-code.js
@@ -1,12 +1,16 @@
 // Help people find their dream jobs!
 
+// Write a function that calculates job awesomeness score
+// based on attributes of a given job offer.
+
 function jobAwesomenessScore(jobAttrs) {
   /**
    * @param {Object} jobAttrs - Qualities of a job tested for awesomeness
-   * @return {number} Score that equals to the number of nice qualities
+   * @return {number} 0 score for offer without good qualities and +1 for every
+   *                  met quality
    */
     const { paidVacation = 0, hammocksInOffice = false, freeLunch = false } = jobAttrs;
-    const decentVacation = 30;
+    const minPaidVacation = 30;
 
     let score = 0;
 
